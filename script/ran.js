9ea3ee6719acd6dfc82d512f28c89446b78841eb
jaydbohol-crypto <jaydbohol@gmail.com>
2026-03-15 22:22:46 +0800

    Update ran.js

--- script/ran.js
+++ script/ran.js
@@ -5,7 +5,7 @@ const path = require("path");
 module.exports.config = {
   name: "ran",
   version: "1.0.0",
-  hasPermssion: 0,
+  role: 2,
   credits: "selov",
   description: "Get random videos",
   commandCategory: "video",
