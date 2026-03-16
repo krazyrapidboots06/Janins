b855c6053a9728534d98725d0ec23edde126e18c
krazyrapidboots06 <krazyrapidboots06@gmail.com>
2026-03-16 21:55:10 +0800

    Update katorsex.js

--- script/katorsex.js
+++ script/katorsex.js
@@ -5,7 +5,7 @@ const path = require("path");
 module.exports.config = {
   name: "katorsex",
   version: "4.0.0",
-  hasPermssion: 0,
+  role: 3,
   credits: "selov",
   description: "Get random video",
   commandCategory: "video",
